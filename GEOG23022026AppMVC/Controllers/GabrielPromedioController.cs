f17b7c6c2daa6cb9d2f272c709b424543fd94603
Gabriel <gabrielobandogarcia100@gmail.com>
2026-02-23 20:04:16 -0600

    Implementación acción GET y POST para cálculo de promedio

--- GEOG23022026AppMVC/Controllers/GabrielPromedioController.cs
+++ GEOG23022026AppMVC/Controllers/GabrielPromedioController.cs
@@ -4,8 +4,17 @@ namespace GEOG23022026AppMVC.Controllers
 {
     public class GabrielPromedioController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(double num1, double num2, double num3, double num4)
+        {
+            double promedio = (num1 + num2 + num3 + num4) / 4;
+            ViewBag.Promedio = promedio;
             return View();
         }
     }

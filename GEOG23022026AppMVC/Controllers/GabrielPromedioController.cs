f237a8043f16e6373dc35c65cf6d7b70282ede01
Gabriel <gabrielobandogarcia100@gmail.com>
2026-02-23 19:55:18 -0600

    Agregar archivos de proyecto.

---
+++ GEOG23022026AppMVC/Controllers/GabrielPromedioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GEOG23022026AppMVC.Controllers
+{
+    public class GabrielPromedioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ GEOG23022026AppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GEOG23022026AppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
